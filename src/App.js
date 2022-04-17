285e691776ef2dab83c4ef64cb162346d1a6417a
samybkd <samybkd@gmail.com>
2022-04-17 14:02:14 +0200

    skeleton site

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import {BrowserRouter as Router,Route, Routes } from "react-router-dom";
+import NavBar from "./components/layout/NavBar";
+import Footer from "./components/layout/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <div className="bg-purple-500">
-      <h1 className='text-xl'>yihaaaaaaa</h1>
-      <button className='btn'>click
-    </button>
-    </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <NavBar/>
+        <main className="Container mx-auto px-3 pb-12">
+            <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path='/about' element={<About/>}/>
+              <Route path='/notfound' element={<NotFound/>}/>
+              <Route path='/*' element={<NotFound/>}/>
+            </Routes>
+        </main>
+        <Footer/>
+      </div>
+
+    </Router>
+
   );
 }
 
---
+++ src/pages/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function About() {
+  return (
+    <>
+        <h1 className="text-6xl mb-4">Github Profile Finder</h1>
+        <p className='mb-4 text-2xl font-light'>
+        A React app to search GitHub profiles and see profile details.
+      </p>
+     
+      <p className='text-lg text-gray-400'>
+        Layout modified By:
+        <p className='text-white'>
+         Samy Bkd
+        </p>
+      </p>
+    
+    </>
+  )
+}
+
+export default About
