475b1547d341c724e0cdc514095ac6036403ca4d
samybkd <samybkd@gmail.com>
2022-04-17 19:38:43 +0200

    fetch users via github api

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useEffect,useState } from 'react'
+
+function UserResults() {
+
+    const [users,setUsers]=useState([])
+    const [isLoading,setIsLoading]=useState(true)
+
+    useEffect(() => {
+      FetchUsers()
+    }, [])
+    
+    const FetchUsers=async()=>{
+        const response=await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+            headers:{
+                Authorization:`token ${process.env.REACT_APP_GITHUB_TOKEN}`
+            }
+        })
+        const data=await response.json()
+        setIsLoading(false)
+        setUsers(data)
+    }
+    
+  if(!isLoading) {
+  
+  return (
+
+    <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-grid-cols-2'>
+        {users.map((user)=>(
+            <h3>{user.login}</h3>
+        ))}
+    </div>
+
+  )
+  }
+  else {
+    return (
+    <h3>is Loading ....</h3>
+    )
+  }
+  
+}
+
+export default UserResults
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import UserResults from '../components/users/UserResults'
 
 function Home() {
   return (
+    <>
+      <UserResults/>
+    </>
     
-    <h1 className="text-6xl">Welcome</h1>
   )
 }
 
